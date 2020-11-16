e2d8ffbcd5993aa519851acd3be492363aee190b
Dannie Greenlee <danniejgreenlee@gmail.com>
2020-11-16 20:37:02 +0000

    add html and js file

---
+++ script.js
@@ -0,0 +1,11 @@
+for (var i=0; i<=100; i++) {
+        if (i === 0) {
+                console.log("even");
+        }
+        else if (i % 2 === 0) {
+                console.log("even");   
+        }
+        else {
+                console.log("odd");
+        }
+}
